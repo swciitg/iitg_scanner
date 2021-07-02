3255f268b9a5bfc49dc28a3cb33f052d0c38239c
kjain333 <karanjain140701@gmail.com>
2021-07-03 01:02:35 +0530

    release changes

--- android/app/src/main/kotlin/com/example/iitg_idcard_scanner/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.iitg_idcard_scanner
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/swc_iitg/iitg_idcard_scanner/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.swc_iitg.iitg_idcard_scanner
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine) // add this line
+    }
+}
+
